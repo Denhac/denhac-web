2ebdc7f4208c2593dd26060c7c28037327c4616d
DESKTOP-QF1EDR5\brian <brian@brianweis.net>
2018-04-24 21:24:28 -0600

    Added Donation Page

--- denhac-web/src/app/app.module.ts
+++ denhac-web/src/app/app.module.ts
@@ -1,22 +1,51 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from "@angular/flex-layout";
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { MainDashComponent } from './main-dash/main-dash.component';
+import { DonateComponent } from './donate/donate.component';
 
+
+const appRoutes: Routes = [
+  {
+    path: 'home',
+    component: MainDashComponent,
+    data: { title: 'Main' }
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'donate',
+    component: DonateComponent
+  }
+  // ,
+  // { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    MainDashComponent,
+    DonateComponent
   ],
   imports: [
     BrowserModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
